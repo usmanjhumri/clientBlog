7b31f855e492ca589e680f7645171575eeca72e7
fahad <mohammadfahadamajd@gmail.com>
2023-10-06 12:52:08 +0500

    added backend api

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express()
+const http = require("http")
+const postRouter = require("./routes/postRouter")
+const cors = require("cors")
+const morgan = require("morgan")
+
+const PORT = process.env.PORT || 3000
+const server = http.createServer(app)
+
+
+app.use(cors())
+app.use(express.json());
+app.use(morgan('tiny'))
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use("/posts", postRouter)
+server.listen(PORT, () => {
+    console.log(`Server ready at: http://localhost:${PORT}`)
+})
